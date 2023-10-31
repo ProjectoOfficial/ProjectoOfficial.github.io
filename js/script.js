9d5003e9d22b3dbca00fcf1153383de24432c5c6
Daniel Rossi <miniprojectsofficial@gmail.com>
2023-10-31 16:15:57 +0100

    Baseline website

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// Recupera il toggle e il body del documento
+const darkModeToggle = document.getElementById("darkModeToggle");
+const body = document.body;
+const container = document.getElementById("main-container");
+
+// Verifica il valore iniziale del toggle
+if (localStorage.getItem("darkMode") === "enabled") {
+    enableDarkMode();
+    darkModeToggle.checked = true;
+}
+
+// Gestisci il cambio di stato del toggle
+darkModeToggle.addEventListener("change", () => {
+    if (darkModeToggle.checked) {
+        enableDarkMode();
+        localStorage.setItem("darkMode", "enabled");
+    } else {
+        disableDarkMode();
+        localStorage.setItem("darkMode", "disabled");
+    }
+});
+
+// Funzioni per abilitare e disabilitare la modalità scura
+function enableDarkMode() {
+    body.classList.add("dark-mode");
+    container.classList.add("dark-mode");
+}
+
+function disableDarkMode() {
+    body.classList.remove("dark-mode");
+    container.classList.remove("dark-mode");
+}
