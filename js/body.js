ec3065fca33b3171e2f60eaaf356974bd25d0600
danielrossi1 <daniel_r@live.it>
2024-08-22 01:45:46 +0200

    fixed some bugs

--- js/body.js
+++ js/body.js
@@ -12,3 +12,10 @@ setInterval(nextSlide, 3000);
 document.getElementById('menuToggle').addEventListener('click', function() {
     document.querySelector('.nav-content').classList.toggle('active');
 });
+
+// Close menu when a link is clicked
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function() {
+        document.querySelector('.nav-content').classList.remove('active');
+    });
+});
